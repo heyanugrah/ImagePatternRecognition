95923853034a47021bdc096036c7d2f285e767c4
Anugrah Bala <sendtoanugrah@gmail.com>
2024-07-13 11:14:26 +0530

    resource and code updated
    working example added

---
+++ example.py
@@ -0,0 +1,4 @@
+import patternIprCode as ipr
+
+output = ipr.createIpr( r'C:\Users\onean\OneDrive\Desktop\image.jpg',r'C:\Users\onean\OneDrive\Desktop\encoded_text_image_lines.png')
+ipr.readIpr(output,r'C:\Users\onean\OneDrive\Desktop\decoded_image.jpg')
